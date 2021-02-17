70f5aab10159447a9d5f8c55635a63208fe0720b
Maran Sowthri Kalailingam <kmaran@athenahealth.com>
2021-02-17 13:51:31 +0530

    Created User model

--- profiles_api/profiles_app/models.py
+++ profiles_api/profiles_app/models.py
@@ -1,3 +1,72 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
-# Create your models here.
+
+class UserProfileManager(BaseUserManager):
+    """Manager for our user model
+
+    Args:
+        BaseUserManager (UserManager): Contributed by django users
+    """
+    def create_user(self, email, name, password):
+        """Creates new user
+
+        Args:
+            email (EmailField): User Email
+            name (CharField): Username
+            password (PasswordField): User Password
+
+        Raises:
+            ValueError: Raise when no email provided
+
+        Returns:
+            User: Created user
+        """
+        if not email:
+            raise ValueError("Email field is required")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+    
+    def create_super_user(self, email, name, password):
+        """Creates super user
+
+        Args:
+            email (EmailField): Super User Email
+            name (CharField): Super User Name
+            password (PasswordField): Super User Password
+
+        Returns:
+            User: Created super user
+        """
+        user = self.create_user(email=email, name=name, password=password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Database model for system user
+
+    Args:
+        AbstractBaseUser (model): Base user provided by django
+        PermissionsMixin (mixin): Mixin to modify user permissions (is_superuser or is_staff etc.)
+    """
+    email = models.EmailField(max_length=50, unique=True)
+    name = models.CharField(max_length=50)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserProfileManager()
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def __str__(self):
+        """String representation of User Model"""
+        return self.name
--- profiles_api/profiles_project/settings.py
+++ profiles_api/profiles_project/settings.py
@@ -121,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Auth User Model
+AUTH_USER_MODEL = 'profiles_app.UserProfile'
